42dc4841eb2bed51cf2134f808e766c67061b0d6
tharanibaskaran <tharanibaskaran0508@gmail.com>
2019-05-16 13:31:27 +0530

    Palindrome added

---
+++ src/testpack/tharani.java
@@ -0,0 +1,31 @@
+package testpack;
+
+import java.util.Scanner;
+
+public class tharani {
+
+	public static void main(String[] args) {
+		  {
+		        String a = "100", b = "";
+		        Scanner s = new Scanner(System.in);
+		        System.out.print("Enter the string to check:");
+		        a = s.nextLine();
+		        int n = a.length();
+		        for(int i = n - 1; i >= 0; i--)
+		        {
+		            b = b + a.charAt(i);
+		        }
+		        if(a.equalsIgnoreCase(b))
+		        {
+		            System.out.println("The string is palindrome.");
+		        }
+		        else
+		        {
+		            System.out.println("The string is not palindrome.");
+		        }
+		    }
+
+	}
+
+}
+
