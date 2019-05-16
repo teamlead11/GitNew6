be86aef0f1942aeb7ca3cb1e97f7e03e313f7be5
saipriya8 <saipriyasusarla@gmail.com>
2019-05-16 12:19:15 +0530

    added to be review pallindrome TC

---
+++ src/testpack/PallindromSaiReview.java
@@ -0,0 +1,24 @@
+package testpack;
+
+import java.util.Scanner;
+
+public class PallindromSaiReview {
+
+	public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+		
+		String original, reverse="";
+		
+		System.out.println("Enter a String to check for Palindrome.");
+		original= sc.nextLine();
+		
+		for(int i= original.length()-1; i>=0;i--) {
+			reverse= reverse + original.charAt(i);
+			
+		}if(original.equals(reverse)) {
+				System.out.println("String Is a pallindrome");
+			}else 
+				System.out.println("String is NOT a pallindrome");
+		}
+
+}
