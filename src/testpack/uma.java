f359491149711524e46768291e7a1ee4caf826da
umathirukumar <sadhasivamuma@gmail.com>
2019-05-19 07:31:59 +0530

    update  testcase

--- src/testpack/uma.java
+++ src/testpack/uma.java
@@ -1,9 +1,24 @@
 package testpack;
 
 public class uma {
+			int id;  
+		    String name;  
+		      
+		        uma(int i,String n){  
+			       id = i;  
+			       name = n;  
+			       }  
+			         
+			       void display(){System.out.println(id+" "+name);}  
+			      
+			       public static void main(String args[]){  
+			       
+			      uma s1 = new uma(111,"Karan");  
+			       uma s2 = new uma(222,"Aryan");  
+			         
+			       s1.display();  
+			       s2.display();  
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
